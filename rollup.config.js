08b42814e730c626a07213f539f548296d7cfea2
Robin van der Vleuten <robin@webstronauts.co>
2017-06-01 17:20:40 +0200

    Simplified build process

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+const babel = require('rollup-plugin-babel');
+const rollup = require('rollup');
+
+const pkg = require('./package.json');
+
+// prettier-ignore
+const banner =
+  '/*!\n' +
+  ' * json-from-script.js v' + pkg.version + '\n' +
+  ' * (c) ' + new Date().getFullYear() + ' Robin van der Vleuten <robin@webstronauts.co>\n' +
+  ' * Released under the MIT License.\n' +
+  ' */'
+
+module.exports = {
+  entry: 'src/index.js',
+  moduleName: 'jsonFromScript',
+  dest: pkg.main,
+  banner,
+  useStrict: false,
+  format: 'iife',
+  plugins: [
+    babel({
+      presets: [['env', { modules: false }], 'flow'],
+      exclude: 'node_modules/**',
+      babelrc: false,
+    }),
+  ],
+};
