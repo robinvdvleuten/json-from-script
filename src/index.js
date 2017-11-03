64de58a45d08cf1ec80bfd4ff8afa5befacc55d2
Robin van der Vleuten <robin@webstronauts.co>
2017-11-03 10:14:56 +0100

    Shaving some bytes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-export default (className, attribute) =>
-  Array.prototype.reduce.call(
-    document.getElementsByClassName(className || 'data'),
-    (data, element) => {
-      data[
-        element.getAttribute(`data-${attribute || 'attribute'}`)
-      ] = JSON.parse(
-        element.textContent
-          .replace(/&amp;/g, '&')
-          .replace(/&quot;/g, '"')
-          .replace(/&#39;/g, "'")
-          .replace(/&lt;/g, '<')
-          .replace(/&gt;/g, '>')
-      );
-
-      return data;
-    },
-    {}
-  );
+module.exports = function(className, attr) {
+  return Array.from(
+    document.getElementsByClassName(className || 'data')
+  ).reduce(function(data, el) {
+    data[el.getAttribute('data-' + (attr || 'attribute'))] = JSON.parse(
+      el.textContent
+        .replace(/&amp;/g, '&')
+        .replace(/&quot;/g, '"')
+        .replace(/&#39;/g, "'")
+        .replace(/&lt;/g, '<')
+        .replace(/&gt;/g, '>')
+    );
+    return data;
+  }, {});
+};
