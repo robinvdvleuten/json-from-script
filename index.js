d5aa9b4a625ae3494b02bca83e1f7980823d8e7d
Robin van der Vleuten <robin@webstronauts.co>
2017-11-04 20:00:39 +0100

    Test the bundle size

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ module.exports = function(className, attr) {
   return Array.from(
     document.getElementsByClassName(className || 'data')
   ).reduce(function(data, el) {
-    data[el.getAttribute('data-' + (attr || 'attribute'))] = JSON.parse(
+    return (data[el.getAttribute('data-' + (attr || 'attribute'))] = JSON.parse(
       el.textContent
         .replace(/&amp;/g, '&')
         .replace(/&quot;/g, '"')
         .replace(/&#39;/g, "'")
         .replace(/&lt;/g, '<')
         .replace(/&gt;/g, '>')
-    );
-    return data;
+    )), data;
   }, {});
 };
